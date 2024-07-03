192ed0b5eb8b7f6d66295acae1d8d9a61aee7af1
jordy rojas <rojas71421@gmail.com>
2024-07-03 14:20:42 +0100

    update dockerfile

--- src/common/exceptions/rpc-custom-exception.filter.ts
+++ src/common/exceptions/rpc-custom-exception.filter.ts
@@ -4,22 +4,31 @@ import { RpcException } from '@nestjs/microservices';
 @Catch(RpcException)
 export class RpcCustomExceptionFilter implements ExceptionFilter {
   catch(exception: RpcException, host: ArgumentsHost) {
-    console.log('paso por aqui en nuestro custom RpcException');
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
     const rpcError = exception.getError();
+
+    if (rpcError.toString().includes('Empty response')) {
+      return response.status(500).json({
+        status: 500,
+        message: rpcError
+          .toString()
+          .substring(0, rpcError.toString().indexOf('(') - 1),
+      });
+    }
+
     if (
       typeof rpcError === 'object' &&
       'status' in rpcError &&
       'message' in rpcError
     ) {
-      const status = rpcError.status;
+      const status = isNaN(+rpcError.status) ? 400 : +rpcError.status;
       return response.status(status).json(rpcError);
     }
 
     response.status(400).json({
       status: 400,
-      messs: rpcError,
+      message: rpcError,
     });
     // return throwError(() => exception.getError());
   }
